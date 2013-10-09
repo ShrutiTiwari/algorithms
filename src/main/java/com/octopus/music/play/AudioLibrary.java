2f88b66671468ae250c0afa65d57438de5bb4015
Shruti Tiwari <reachstiwari@gmail.com>
2013-10-09 18:02:22 +0100

    Modified Handling.

--- src/main/java/com/octopus/music/play/AudioLibrary.java
+++ src/main/java/com/octopus/music/play/AudioLibrary.java
@@ -5,19 +5,13 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
-import com.octopus.music.play.AudioPlayer.CommandExecutor;
 import com.octopus.music.play.AudioPlayer.VLCPlayer;
 
 public class AudioLibrary {
-    static final String VLC_EXE_LOCATION_WINDOWS = "C:/software/VideoLAN/VLC/vlc.exe";
-    static final String VLC_EXE_LOCATION_LINUX = "/usr/bin/vlc-wrapper";
-	
-    //private static final String AUDIO_LIBRARY = "C:\\recognition-puzzles\\";
 	private static final String AUDIO_LIBRARY = "recognition-puzzles/";
 	private static final String FOLDER_PREFIX = "note-recognition-";
-	static Map<String, File> allNoteAudios;
-	
-	private static AudioPlayer audioPlayer= new VLCPlayer( CommandExecutor.WINDOWS, VLC_EXE_LOCATION_WINDOWS );
+	private static Map<String, File> allNoteAudios;
+	private static AudioPlayer audioPlayer= new VLCPlayer( );
 
 	public static AudioPlayer audioPlayer(){
 	    return audioPlayer;
@@ -25,10 +19,6 @@ public class AudioLibrary {
 	
 	public static void initializeWithGivenSeconds(int seconds) {
 		AudioLibrary.allNoteAudios = findAllNotesAudios(seconds);
-		String os = System.getProperty("os.name");
-		if(!os.contains("Windows")){
-			audioPlayer= new VLCPlayer( CommandExecutor.WINDOWS, VLC_EXE_LOCATION_LINUX );
-		}
 	}
 
 	static void addFileIfFound(List<File> audioFiles, Playable note) {
--- src/main/java/com/octopus/music/play/AudioPlayer.java
+++ src/main/java/com/octopus/music/play/AudioPlayer.java
@@ -1,79 +1,77 @@
 package com.octopus.music.play;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.List;
 
-public interface AudioPlayer
-{
-    void playList( List<File> audioFiles );
+public interface AudioPlayer {
+	static final String VLC_EXE_LOCATION_WINDOWS = "C:/software/VideoLAN/VLC/vlc.exe";
+	static final String VLC_EXE_LOCATION_LINUX = "/usr/bin/vlc-wrapper";
 
-    public class VLCPlayer implements AudioPlayer
-    {
-        private static final String vlcOption = "--play-and-exit";
-        private final CommandExecutor commandExecutor;
-        private final String vlcExeLoc;
+	void playList(List<File> audioFiles);
 
-        VLCPlayer( CommandExecutor commandExecutor, String vlcExeLoc ) {
-            this.commandExecutor = commandExecutor;
-            this.vlcExeLoc = vlcExeLoc;
-        }
+	public class VLCPlayer implements AudioPlayer {
+		private static final String vlcOption = "--play-and-exit";
+		private final String vlcExeLoc;
 
-        public void playList( List<File> audioFiles ) {
-            play( audioFiles.toArray( new File[audioFiles.size()] ) );
-        }
+		VLCPlayer() {
+			this.vlcExeLoc = findAudioPlayerBasedOnOS();
+		}
 
-        private void play( File... audioFiles ) {
-            String[] command = new String[2 + audioFiles.length];
-            command[0] = vlcExeLoc;
-            command[1] = vlcOption;
-            int i = 0;
-            for( File each : audioFiles ) {
-                command[i++ + 2] = each.getAbsolutePath();
-            }
-            commandExecutor.executeCommand( command );
-        }
-    }
-    
-    interface CommandExecutor
-    {
-        void executeCommand(String[] command);
+		public void playList(List<File> audioFiles) {
+			play(audioFiles.toArray(new File[audioFiles.size()]));
+		}
 
-        public static CommandExecutor WINDOWS= new WindowsCommandExecutor();
-        
-        class WindowsCommandExecutor implements CommandExecutor
-        {
-            private final Runtime runtime = Runtime.getRuntime();
-            
-            public void executeCommand(String[] command) {
-                try {
-                	for(String each: command){
-                		System.out.println(each);
-                	}
-                	
-                    Process p = runtime.exec( command );
-                    int success = p.waitFor();
-                    if( success != 0 ) {
-                        printError(p);
-                    }
-                } catch( IOException e ) {
-                    e.printStackTrace();
-                } catch( InterruptedException e ) {
-                    e.printStackTrace();
-                }
-            }
-
-			private void printError(Process p) throws IOException {
-				System.out.println( "process execution failed " + p.exitValue() );
-				BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(p.getErrorStream()));
-				String s=null;
-				while((s=bufferedReader.readLine())!=null){
-				System.out.println( s);
-				}
+		private void play(File... audioFiles) {
+			String[] command = new String[2 + audioFiles.length];
+			command[0] = vlcExeLoc;
+			command[1] = vlcOption;
+			int i = 0;
+			for (File each : audioFiles) {
+				command[i++ + 2] = each.getAbsolutePath();
 			}
-        }
-    }
+			new CommandExecutor().executeCommand(command);
+		}
+		
+		private String findAudioPlayerBasedOnOS() {
+			String os = System.getProperty("os.name");
+			return (!os.contains("Windows")) ? VLC_EXE_LOCATION_LINUX
+					: VLC_EXE_LOCATION_WINDOWS;
+		}
+	}
+
+	class CommandExecutor {
+		private final Runtime runtime = Runtime.getRuntime();
+
+		public void executeCommand(String[] command) {
+			try {
+				for (String each : command) {
+					System.out.println(each);
+				}
+
+				Process p = runtime.exec(command);
+				int success = p.waitFor();
+				if (success != 0) {
+					printError(p);
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+		private void printError(Process p) throws IOException {
+			System.out.println("process execution failed " + p.exitValue());
+			BufferedReader bufferedReader = new BufferedReader(
+					new InputStreamReader(p.getErrorStream()));
+			String s = null;
+			while ((s = bufferedReader.readLine()) != null) {
+				System.out.println(s);
+			}
+		}
+	}
+	
 }
