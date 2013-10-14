9550ca9ed2fada2fc6aba9f6e723055efd4d56d2
Shruti Tiwari <reachstiwari@gmail.com>
2013-10-14 18:09:45 +0100

    tidyingup.

--- music/src/test/java/com/aqua/music/items/SymmetricalPatternApplicatorTest.java
+++ music/src/test/java/com/aqua/music/items/SymmetricalPatternApplicatorTest.java
@@ -10,6 +10,6 @@ public class SymmetricalPatternApplicatorTest
     public void simpleTest() {
         SymmetricalPatternApplicator<String> patternApplicator = new SymmetricalPatternApplicator<String>( new int[] { 1, 2 } );
         patternApplicator.generateAscendAndDescendSequences( new String[] { "A1", "A2", "A3" } );
-        assertEquals( "A1A2, A2A3" + PatternApplicator.SEP + "A3A2, A2A1", patternApplicator.prettyPrintTextForAscDesc() );
+        assertEquals( "[1-2]==>A1A2, A2A3" + PatternApplicator.SEP + "A3A2, A2A1", patternApplicator.prettyPrintTextForAscDesc() );
     }
 }
