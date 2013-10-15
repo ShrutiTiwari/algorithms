74b98b1b41c23a8c6563fabd810618771b916df6
Shruti Tiwari <reachstiwari@gmail.com>
2013-10-15 18:34:25 +0100

    tidying further.

--- music/src/main/java/com/aqua/music/ui/DisplayItemFactory.java
+++ music/src/main/java/com/aqua/music/ui/DisplayItemFactory.java
@@ -1,8 +1,13 @@
 package com.aqua.music.ui;
 
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+
+import com.aqua.music.items.PlayableItem;
+import com.aqua.music.model.FrequencySet;
 
 public class DisplayItemFactory {
 	static final int HORIZONAL_COORIDNATE = 30;
@@ -14,10 +19,43 @@ public class DisplayItemFactory {
 	private int verticalIndex = 10;
 
 	JButton createWith(DisplayItemType buttonType, Object arg) {
-		JButton displayItem = buttonType.creteDisplayItem(arg);
-		verticalIndex += (BUTTON_HEIGHT) + 10;
-		displayItem.setBounds(HORIZONAL_COORIDNATE, verticalIndex,
+		JButton displayItem = buttonType.createInstanceWith(arg);
+		displayItem.setBounds(HORIZONAL_COORIDNATE, verticalIndex(),
 				buttonType.width(), BUTTON_HEIGHT);
 		return displayItem;
+	}
+
+	private int verticalIndex() {
+		verticalIndex += (BUTTON_HEIGHT) + 10;
+		return verticalIndex;
+	}
+	
+	static class ActionListenerBuilder {
+		private final Object arg;
+
+		ActionListenerBuilder(Object arg) {
+			this.arg = arg;
+		}
+
+		ActionListener actionListener(DisplayItemType displayItemType) {
+			switch (displayItemType) {
+			case PLAYABLE:
+				return new ActionListener() {
+					@Override
+					public void actionPerformed(ActionEvent arg0) {
+						PlayableItem.factory.forSet((FrequencySet) arg)
+								.playWithoutBlocking();
+					}
+				};
+			case QUIT:
+				return new ActionListener() {
+					@Override
+					public void actionPerformed(ActionEvent arg0) {
+						System.exit(0);
+					}
+				};
+			}
+			return null;
+		}
 	}
 }
--- music/src/main/java/com/aqua/music/ui/DisplayItemType.java
+++ music/src/main/java/com/aqua/music/ui/DisplayItemType.java
@@ -1,77 +1,57 @@
 package com.aqua.music.ui;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
 import javax.swing.JButton;
 
-import com.aqua.music.items.PlayableItem;
 import com.aqua.music.model.FrequencySet;
 
 public enum DisplayItemType {
 	PLAYABLE("Play $$", "Click this to play $$", 200) {
 		@Override
-		public JButton creteDisplayItem(Object arg) {
-			String setName = ((FrequencySet) arg).name();
-			JButton playableButton = new JButton(this.text.replace("$$",
-					setName));
-			playableButton.setToolTipText(tooltip.replace("$$", setName));
-			playableButton.addActionListener(new ActionListenerBuilder(arg)
-					.playableItemsActionListener());
-			return playableButton;
+		public JButton createInstanceWith(Object arg) {
+			return dynamicButton(this, arg, ((FrequencySet) arg).name());
 		}
 	},
 	QUIT("Quit", "Click this to quit!", 400) {
 		@Override
-		public JButton creteDisplayItem(Object arg) {
-			JButton quitButton = new JButton(text);
-			quitButton.setToolTipText(tooltip);
-			quitButton.addActionListener(new ActionListenerBuilder(arg)
-					.quitActionListener());
-			return quitButton;
+		public JButton createInstanceWith(Object arg) {
+			staticButton.addActionListener(new DisplayItemFactory.ActionListenerBuilder(null)
+			.actionListener(this));
+			return this.staticButton;
 		}
 	};
-	final String text;
-	final String tooltip;
+	private final String text;
+	private final String tooltip;
 	private final int displayWidth;
+	JButton staticButton;
 
 	DisplayItemType(String text, String tooltip, int buttonWidth) {
 		this.text = text;
 		this.tooltip = tooltip;
 		this.displayWidth = buttonWidth;
+
+		if (!text.contains("$$")) {
+			this.staticButton = new JButton(text);
+			staticButton.setToolTipText(tooltip);
+		}
 	}
 
-	public abstract JButton creteDisplayItem(Object arg);
+	public abstract JButton createInstanceWith(Object arg);
+
+	private static JButton dynamicButton(DisplayItemType itemType, Object arg,
+			String replaceName) {
+		JButton resultButton = new JButton(itemType.text.replace("$$",
+				replaceName));
+		resultButton
+				.setToolTipText(itemType.tooltip.replace("$$", replaceName));
+		resultButton.addActionListener(new DisplayItemFactory.ActionListenerBuilder(arg)
+				.actionListener(itemType));
+		return resultButton;
+	}
 
 	public int width() {
 		return displayWidth;
 	}
 
-	public class ActionListenerBuilder {
-		private final Object arg;
 
-		private ActionListenerBuilder(Object arg) {
-			this.arg = arg;
-		}
-
-		private ActionListener playableItemsActionListener() {
-			return new ActionListener() {
-				@Override
-				public void actionPerformed(ActionEvent arg0) {
-					PlayableItem.factory.forSet((FrequencySet) arg)
-							.playWithoutBlocking();
-				}
-			};
-		}
-
-		private ActionListener quitActionListener() {
-			return new ActionListener() {
-				@Override
-				public void actionPerformed(ActionEvent arg0) {
-					System.exit(0);
-				}
-			};
-		}
-	}
 
 }
