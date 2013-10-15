a833760c71d48d39b8711059dc792e4e7dcfd3b6
Acer4736 <Acer4736@Nerd>
2013-10-16 00:07:56 +0100

    Failed attempt on applet.

---
+++ music/src/main/java/LauncherApplet.java
@@ -0,0 +1,28 @@
+import java.awt.BorderLayout;
+
+import javax.swing.JApplet;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+import com.aqua.music.ui.GuiMultitabPanel;
+import com.aqua.music.ui.SwingGuiLauncher;
+
+public class LauncherApplet extends JApplet {
+	public void init() {
+		try {
+			//SwingGuiLauncher.main(null);
+			SwingUtilities.invokeAndWait(new Runnable() {
+				@Override
+				public void run() {
+					//JLabel lab = new JLabel("Hello World, Shruti");
+					//add(new GuiMultitabPanel(), BorderLayout.CENTER );
+					JFrame jframe = new SwingGuiLauncher().createAndShowGUI();
+					//add(jframe);
+					setContentPane(new GuiMultitabPanel());
+				}
+			});
+		} catch (Exception e) {
+			System.err.println("createGUI failed");
+		}
+	}
+}
