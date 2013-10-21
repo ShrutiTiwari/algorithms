177a9368ae5c5b3d2d633a504a8d460b7dd1c247
Shruti Tiwari <reachstiwari@gmail.com>
2013-10-21 19:49:29 +0100

    applet inside package.

--- music/src/main/java/com/aqua/music/ui/LauncherApplet.java
+++ music/src/main/java/com/aqua/music/ui/LauncherApplet.java
@@ -1,21 +1,15 @@
+package com.aqua.music.ui;
 import javax.swing.JApplet;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
-import com.aqua.music.ui.GuiMultitabPanel;
-import com.aqua.music.ui.SwingGuiLauncher;
-
 public class LauncherApplet extends JApplet {
 	public void init() {
 		try {
-			//SwingGuiLauncher.main(null);
 			SwingUtilities.invokeAndWait(new Runnable() {
 				@Override
 				public void run() {
-					//JLabel lab = new JLabel("Hello World, Shruti");
-					//add(new GuiMultitabPanel(), BorderLayout.CENTER );
 					JFrame jframe = new SwingGuiLauncher().createAndShowGUI();
-					//add(jframe);
 					setContentPane(new GuiMultitabPanel());
 				}
 			});
