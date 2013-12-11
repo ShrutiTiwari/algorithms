2afb4454e718cdcfefae42f897baf596a533184c
"Shruti Tiwari" <"Shruti Tiwari"@Nerd>
2013-12-11 19:48:49 +0000

    display changes.

--- music/music-view/src/main/java/com/aqua/music/view/components/UiTabbedPane.java
+++ music/music-view/src/main/java/com/aqua/music/view/components/UiTabbedPane.java
@@ -14,22 +14,21 @@ import com.aqua.music.model.puzzles.QuizLevel;
  * @author "Shruti Tiwari"
  */
 public class UiTabbedPane {
-	private static final String TITLE_QUIZ_TAB = "Quiz with Frequency sets";
-	private static final String TITLE_REHEARSE_TAB = "Rehearse with pattern";
 	private static final QuizLevel<CyclicFrequencySet> firstQuizLevel = (QuizLevel<CyclicFrequencySet>) QuizController.FrequencySetQuiz
 			.quizLevels().iterator().next();
 	private static final SymmetricalSet firstThaat = SymmetricalSet.THAAT_KAFI;
 
-	private static final String TITLE_PLAIN_THAAT = "Thaat rehearse";
-	private static final String TITLE_SONG_TAB = "Song rehearse";
-
+	private static final String TITLE_QUIZ_TAB = "Thaat Puzzles";
+	private static final String TITLE_SONG_TAB = "Practice songs";
+	private static final String TITLE_PRACTICE_THAAT_PATTERN = "Practice Thaats with pattern";
+	private static final String TITLE_PRACTICE_THAAT_PLAIN = "Practice Thaats";
 
 	public static JTabbedPane getTabbedPane() {
 		JTabbedPane tabbedPane = new JTabbedPane();
-		tabbedPane.addTab(UiTabbedPane.TITLE_PLAIN_THAAT, new MusicPanelForPractice(PlayApi.getAllPlainThaat()).getPanel());
-		tabbedPane.addTab(UiTabbedPane.TITLE_SONG_TAB, new MusicPanelForPractice(PlayApi.getAllSongs()).getPanel());
-		tabbedPane.addTab(TITLE_REHEARSE_TAB, new MusicPanelForPractice(firstThaat, PermuatationsGenerator.PAIR).getPanel());
+		tabbedPane.addTab(UiTabbedPane.TITLE_PRACTICE_THAAT_PLAIN, new MusicPanelForPractice(PlayApi.getAllPlainThaat()).getPanel());
+		tabbedPane.addTab(TITLE_PRACTICE_THAAT_PATTERN, new MusicPanelForPractice(firstThaat, PermuatationsGenerator.PAIR).getPanel());
 		tabbedPane.addTab(TITLE_QUIZ_TAB, new MusicPanelForQuiz(firstQuizLevel).getPanel());
+		tabbedPane.addTab(UiTabbedPane.TITLE_SONG_TAB, new MusicPanelForPractice(PlayApi.getAllSongs()).getPanel());
 		tabbedPane.setOpaque(true);
 		return tabbedPane;
 	}
