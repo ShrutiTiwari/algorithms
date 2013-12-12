a873904402882c01ebddc2513593dada38664255
"Shruti Tiwari" <"Shruti Tiwari"@Nerd>
2013-12-12 23:36:08 +0000

    display changes.

--- music/music-view/src/main/java/com/aqua/music/view/components/CommonTopPanel.java
+++ music/music-view/src/main/java/com/aqua/music/view/components/CommonTopPanel.java
@@ -9,6 +9,7 @@ import java.awt.Component;
 import javax.swing.Box;
 import javax.swing.JButton;
 import javax.swing.JComponent;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import com.aqua.music.view.components.UiButtons.MusicButtons;
@@ -17,12 +18,12 @@ public class CommonTopPanel {
 	private final JButton pauseButton;
 	private final JPanel leftPanel = UiJPanelBuilder.LEFT_FLOWLAYOUT.createPanel();
 	private JPanel rightPanel;
-	private final JPanel topMostPanel = UiJPanelBuilder.BOX_HORIZONTAL.createPanel();
+	private final JPanel mainPanel = UiJPanelBuilder.BOX_HORIZONTAL.createPanel();
 
-	public JButton pauseButton(){
+	public JButton pauseButton() {
 		return pauseButton;
 	}
-	
+
 	public CommonTopPanel() {
 		this.pauseButton = MusicButtons.PAUSE.createStaticNamedButton();
 		this.rightPanel = UiJPanelBuilder.RIGHT_FLOWLAYOUT.createPanel();
@@ -31,8 +32,11 @@ public class CommonTopPanel {
 		addToPanel(MusicButtons.DECREASE_TEMPO.createStaticNamedButton(), rightPanel);
 		addToPanel(pauseButton, rightPanel);
 
-		topMostPanel.add(leftPanel);
-		topMostPanel.add(rightPanel);
+		mainPanel.add(leftPanel);
+		
+		leftPanel.add(new JLabel(" Choose one of the practice tabs for practice "));
+		
+		mainPanel.add(rightPanel);
 	}
 
 	private static void addToPanel(JComponent each, JPanel containerPanel) {
@@ -42,11 +46,7 @@ public class CommonTopPanel {
 		containerPanel.add(Box.createVerticalGlue());
 	}
 
-	public void add(JComponent aComponent) {
-		leftPanel.add(aComponent);
-	}
-
 	public JPanel getPanel() {
-		return topMostPanel;
+		return mainPanel;
 	}
 }
