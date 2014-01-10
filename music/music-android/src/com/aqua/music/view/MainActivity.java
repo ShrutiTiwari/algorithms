665e3da588051ef15f2cec94ac96e778e639a22c
ShrutiTiwari <reachstiwari@gmail.com>
2014-01-10 22:22:23 +0000

    new features.

--- music/music-android/src/com/aqua/music/view/MainActivity.java
+++ music/music-android/src/com/aqua/music/view/MainActivity.java
@@ -1,35 +1,23 @@
 package com.aqua.music.view;
 
-import java.util.List;
-
-import open.music.api.Playable;
-import open.music.api.SingletonFactory;
 import android.content.Intent;
-import android.os.Bundle;
 import android.view.View;
-import android.widget.AbsListView;
+import android.widget.ListAdapter;
 
-public class MainActivity extends SoundPlayActivity {
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		intializeAudioSystem();
-		populateList();
-	}
+public class MainActivity extends BaseSoundPlayActivity {
+	private final int activityId = R.layout.activity_main;
 
-	private void populateList() {
-		final AbsListView listView = (AbsListView) findViewById(R.id.list);
-		List<Playable> allPlainThaat = (List<Playable>) SingletonFactory.PLAY_API
-				.getAllPlainThaat();
-
-		final PlyablesArrayAdapter adapter = new PlyablesArrayAdapter(this,
-				R.layout.list_item, allPlainThaat);
-		listView.setAdapter(adapter);
-		thaatClickListener = new PlyableClickListener(this);
-		listView.setOnItemClickListener(thaatClickListener);
-	}
 	public void playPattern(View view) {
 		startActivity(new Intent(this, PatternActivity.class));
+	}
+
+	@Override
+	protected int getActivityId() {
+		return activityId;
+	}
+
+	@Override
+	protected ListAdapter getDataAdapter() {
+		return new PlyablesArrayAdapter(this, R.layout.list_item, allThaats);
 	}
 }
